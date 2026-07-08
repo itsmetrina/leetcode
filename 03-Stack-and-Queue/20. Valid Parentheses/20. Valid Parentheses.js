6d6d7a65881555e748df3344fcbe605d9314ecea
Trina <76439178+itsmetrina@users.noreply.github.com>
2026-07-08 23:34:46 +0530

    [20] [Time Beats: 50.80%] [Memory Beats: 44.27%] - Leet2Hub

--- 03-Stack-and-Queue/20. Valid Parentheses/20. Valid Parentheses.js
+++ 03-Stack-and-Queue/20. Valid Parentheses/20. Valid Parentheses.js
@@ -1,1 +1,24 @@
-        if(char === '(' || char === '{' || char === '[') {            stack.push(char);        } else {            if(stack[stack.length - 1] === map[char]) {                stack.pop();            } else return false;        }    }    return stack.length === 0;};
+1/**
+2 * @param {string} s
+3 * @return {boolean}
+4 */
+5var isValid = function(s) {
+6    const stack = [];
+7    const map = {
+8        ')' : '(',
+9        '}' : '{',
+10        ']' : '['
+11    };
+12
+13    for (let char of s) {
+14        if(char === '(' || char === '{' || char === '[') {
+15            stack.push(char);
+16        } else {
+17            if(stack[stack.length - 1] === map[char]) {
+18                stack.pop();
+19            } else return false;
+20        }
+21    }
+22
+23    return stack.length === 0;
+24};
