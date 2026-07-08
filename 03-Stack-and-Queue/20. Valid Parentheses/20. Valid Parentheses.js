040581805ea2d76e0afe0c428ff119663d7acdfd
Trina <76439178+itsmetrina@users.noreply.github.com>
2026-07-08 23:34:38 +0530

    [20] [Time Beats: N/A] [Memory Beats: N/A] - Leet2Hub

---
+++ 03-Stack-and-Queue/20. Valid Parentheses/20. Valid Parentheses.js
@@ -0,0 +1,1 @@
+        if(char === '(' || char === '{' || char === '[') {            stack.push(char);        } else {            if(stack[stack.length - 1] === map[char]) {                stack.pop();            } else return false;        }    }    return stack.length === 0;};
